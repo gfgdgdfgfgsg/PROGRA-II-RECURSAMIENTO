9e28451dcfbb6b0e5c7436fbc3375554ddc5e4ee
hugopatiñooo19 <133272436+gfgdgdfgfgsg@users.noreply.github.com>
2024-10-17 11:26:54 -0600

    Create Ejecutor.java

---
+++ PRACTICAS/PRIMER PARCIAL/Clase Abstracta/Ejecutor.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.IOException;
+
+public class Ejecutor extends TareaPeriodica {
+
+    private String comando;
+
+    public Ejecutor(String comando, int periodo) {
+        super(periodo);
+        this.comando = comando;
+    }
+
+    public boolean necesitaEjecucion() {
+        if (!activa) return false;
+        // Calcula la hora de la próxima ejecución
+        Calendar proximaEjecucion = new GregorianCalendar();
+        proximaEjecucion.setTime(ultimaEjecucion);
+        proximaEjecucion.add(Calendar.SECOND, periodo);
+        Calendar ahora = new GregorianCalendar();
+        // Comprobamos si ha pasado a la hora actual
+        return (proximaEjecucion.before(ahora));
+    }
+
+    public int ejecutarTarea() {
+        try {
+            Runtime.getRuntime().exec(comando);
+            return 0;
+        } catch (IOException e) {
+            System.err.println(e.toString());
+        }
+        return -1;
+    }
+
+    public String leerComando() {
+        return comando;
+    }
+}
