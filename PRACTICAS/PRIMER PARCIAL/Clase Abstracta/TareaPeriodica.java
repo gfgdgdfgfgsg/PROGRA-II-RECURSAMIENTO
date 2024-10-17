b1d24e97042da1d0ef4578fcf2894cd41057d98d
hugopatiñooo19 <133272436+gfgdgdfgfgsg@users.noreply.github.com>
2024-10-17 11:26:07 -0600

    Create TareaPeriodica.java

---
+++ PRACTICAS/PRIMER PARCIAL/Clase Abstracta/TareaPeriodica.java
@@ -0,0 +1,6 @@
+public abstract class TareaPeriodica {
+    // Otros atributos y métodos
+
+    public abstract void necesitaEjecucion(); // Método abstracto sin cuerpo
+    public abstract void ejecutarTarea();      // Método abstracto sin cuerpo
+}
